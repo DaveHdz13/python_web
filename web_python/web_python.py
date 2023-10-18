ff27c70c025e8fe1d2964b5b3a2fc8fc162d7c5f
DaveHdz <76536169+DaveHdz13@users.noreply.github.com>
2023-10-18 13:32:20 -0600

    first commit

---
+++ web_python/web_python.py
@@ -0,0 +1,52 @@
+import reflex as rx
+from web_python import style
+
+def qa(question: str, answer: str) -> rx.Component:
+  return rx.box(
+    rx.box(
+      rx.text(question, style=style.question_style),
+      text_align="right"
+    ),
+    rx.box(
+      rx.text(answer, style=style.answer_style),
+      text_align="left"
+    ),
+    margin_y="1em",
+  )
+
+def chat() -> rx.Component:
+  qa_pairs = [
+    (
+      "What is reflex?",
+      "A way to build web apps in pure Python!",
+    ),
+    (
+      "What can I make with it?",
+      "Anything from a simple website to a complex web app!",
+    )
+  ]
+  return rx.box(
+    *[
+      qa(question, answer)
+      for question, answer in qa_pairs
+    ]
+  )
+
+def action_bar() -> rx.Component:
+  return rx.hstack(
+        rx.input(
+            placeholder="Ask a question",
+            style=style.input_style,
+        ),
+        rx.button("Ask", style=style.button_style),
+    )
+
+def index() -> rx.Component:
+  return rx.container(
+    chat(),
+    action_bar(),
+  )
+
+app = rx.App()
+app.add_page(index)
+app.compile()
